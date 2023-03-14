c52b71aa5ff23b99c119a43b94538920562242a4
SebastianWolter1 <seb@techtakel.berlin>
2023-03-14 15:12:33 +0100

    new components

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,14 +1,27 @@
-import Footer from "./Footer";
-import Header from "./Header";
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-const Layout = ({ children }) => {
+const Navbar = () => {
+
+
+  const items = [
+    {name: "Home", to: "/"},
+    {name: "Add Item", to: "/add-item"},
+    {name: "Basket", to: "/basket"},
+  ]
   return (
-    <>
-      <Header />
-      {children}
-      <Footer />
-    </>
-  );
-};
+    <nav>
+          <ul className="ul">
+            {items.map((item) => {
+              return (
+                <li key={item.to}>
+                  <NavLink to={item.to}>{item.name}</NavLink>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+  )
+}
 
-export default Layout;
+export default Navbar;
